462ce6620271c38225d4fb1819a8b48a3d1aa9ba
jakeowns <beanshield@gmail.com>
2016-12-27 15:09:47 -0800

    Add AsyncSubject to cache fetchRoute.

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable, AsyncSubject } from 'rxjs/Rx';
 import api from '../assets/weatherApiConfig';
 
 import 'rxjs/add/operator/map';
@@ -10,15 +10,21 @@ import 'rxjs/add/operator/catch';
 export class WeatherService {
 
   private apiRoute: string = `${api.domain}${api.key}${api.tideForecastRoute}`;
+  private subject: AsyncSubject<{}>;
 
   constructor(private http: Http) { }
 
   fetchRoute(transform) {
-    return this.http
-      .get(this.apiRoute)
-      .map((resp: Response) => transform(resp.json()))
-      .catch((error: any) =>
-        Observable.throw(error.json().error || 'WeatherService fetchRoute error'));
+    return Observable.create((observer) => {
+      if (!this.subject) {
+        this.subject = new AsyncSubject();
+        this.http.get(this.apiRoute).subscribe(this.subject);
+      }
+      return this.subject.subscribe(observer);
+    })
+    .map((resp: Response) => transform(resp.json()))
+    .catch((error: any) =>
+      Observable.throw(error.json().error || 'WeatherService fetchRoute error'));
   }
 
   getTide() {
